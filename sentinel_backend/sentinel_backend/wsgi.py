ff0f29312342f5e3a465a758846d857873fc64c8
sony-level <sonydilane@gmail.com>
2024-02-12 01:18:01 +0100

    initialization

--- sentinel_backend/sentinel_backend/wsgi.py
+++ sentinel_backend/sentinel_backend/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
+"""    
 
 import os
 
